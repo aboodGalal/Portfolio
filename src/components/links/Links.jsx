37c3a112b8ac6859eb11d19649a0415d38eca762
Your Name <you@example.com>
2024-04-14 22:16:36 +0200

    change the navbar position

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -14,8 +14,8 @@ function Links({ navOpen, setNavOpen }) {
 
   return (
     <ul
-      className={` md:flex md:flex-row md:items-center md:static md:w-auto absolute
-    left-[50%] transform -translate-x-1/2 md:-translate-x-[0px] md:py-0 md:pl-0 pl-0 pt-14 md:pt-0 md:opacity-100
+      className={` md:flex md:flex-row md:items-center md:static md:w-auto fixed
+      left-[50%] transform -translate-x-1/2 md:-translate-x-[0px] md:py-0 md:pl-0 pl-0 pt-14 md:pt-0 md:opacity-100
       ${navOpen ? 'top-[20px] w-[95%] h-[310px]' : 'w-[0px] h-[0px] md:h-full top-[100px]  opacity-0'}
        origin-center  sm:z-50 md:z-[400]
        transition-all ease-in duration-200 bg-[#14191b] md:bg-transparent overflow-hidden rounded-lg`}
@@ -24,20 +24,17 @@ function Links({ navOpen, setNavOpen }) {
         <li
           key={link}
           className={`hover:md:translate-x-0 hover:translate-x-6 transition-all duration-500 ml-8 
-    ${link === 'Contact me' ? 'border-b-[0px] ' : 'border-b-[1px]'} 
-    li md:border-b-[0px] border-gray-700`}
-        >
+          ${link === 'Contact me' ? 'border-b-[0px] ' : 'border-b-[1px]'} 
+          li md:border-b-[0px] border-gray-700`}>
           <a
             className={`py-4 mx-0 md:my-0 text-lg block w-full h-full a 
-      hover:text-[deepskyblue] transition-all duration-200 relative text-white ${link}`}
+          hover:text-[deepskyblue] transition-all duration-200 relative text-white ${link}`}
             href={`#${link}`}
-            onClick={() => handleScroll(link)}
-          >
+            onClick={() => handleScroll(link)}>
             {link}
           </a>
         </li>
       ))}
-
     </ul>
   )
 }
